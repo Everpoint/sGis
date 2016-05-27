ae14b4bb06cc5013d6c4522adf503bf067f899cc
Maxim Gritsenko <maxim@gritsenko.biz>
2016-05-27 15:30:41 +0900

    SGIS-69 JSDoc for sGis.Bbox

--- source/Bbox.js
+++ source/Bbox.js
@@ -10,10 +10,17 @@ sGis.module('Bbox', [
     };
 
     /**
+     * Simple rectangular area on a map
      * @class
      * @alias sGis.Bbox
      */
     class Bbox {
+        /**
+         * @constructor
+         * @param {sGis.Point|number[]} point1
+         * @param {sGis.Point|number[]} point2
+         * @param {sGis.Crs} [crs=sGis.CRS.geo]
+         */
         constructor(point1, point2, crs)
         {
             this._crs = crs || point1.crs || point2.crs || this._crs;
@@ -23,55 +30,102 @@ sGis.module('Bbox', [
             this._p = [Math.min(p1[0], p2[0]), Math.min(p1[1], p2[1]), Math.max(p1[0], p2[0]), Math.max(p1[1], p2[1])];
         }
 
+        /**
+         * Returns a new Bbox in the specified coordinate system
+         * @param {sGis.Crs} crs - target coordinate system
+         * @returns {sGis.Bbox}
+         */
         projectTo(crs) {
             return new Bbox(new Point(this._p[0], this._p[1], this._crs), new Point(this._p[2], this._p[3], this._crs), crs);
         }
 
+        /**
+         * Returns a copy of the bbox
+         * @returns {sGis.Bbox}
+         */
         clone() {
             return this.projectTo(this._crs);
         }
 
+        /**
+         * Returns true if the given bbox is equal (geographically) to the target bbox
+         * @param {sGis.Bbox} bbox - target bbox
+         * @returns {boolean}
+         */
         equals(bbox) {
             var target = bbox.coordinates;
             for (var i = 0; i < 4; i++) if (this._p[i] !== target[i]) return false;
             return this._crs === bbox.crs;
         }
 
+        /**
+         * Returns true if at list one point of the given bbox lies inside the target bbox
+         * @param {sGis.Bbox} bbox - target bbox
+         * @returns {boolean}
+         */
         intersects(bbox) {
             var proj = bbox.projectTo(this._crs);
             return this.xMax > proj.xMin && this.xMin < proj.xMax && this.yMax > proj.yMin && this.yMin < proj.yMax;
         }
 
+        /**
+         * Coordinate system of the bbox
+         * @type sGis.Crs
+         */
         get crs() { return this._crs; }
 
+        /**
+         * Right border of the bbox
+         */
         get xMax() { return this._p[2] }
         set xMax(value) {
             if (value < this.xMin) utils.error('Max value cannot be lower than the min value');
             this._p[2] = value;
         }
 
+        /**
+         * Top border of the bbox
+         */
         get yMax() { return this._p[3]; }
         set yMax(value) {
             if (value < this.yMin) sGis.utils.error('Max value cannot be lower than the min value');
             this._p[3] = value;
         }
 
+        /**
+         * Left border of the bbox
+         */
         get xMin() { return this._p[0]; }
         set xMin(value) {
             if (value > this.xMax) sGis.utils.error('Min value cannot be higher than the max value');
             this._p[0] = value;
         }
 
+        /**
+         * Bottom border of the bbox
+         */
         get yMin() { return this._p[1]; }
         set yMin(value) {
             if (value > this.yMax) sGis.utils.error('Min value cannot be higher than the max value');
             this._p[1] = value;
         }
 
+        /**
+         * Width of the bbox
+         * @type number
+         */
         get width() { return this.xMax - this.xMin; }
 
+        /**
+         * Height of the bbox
+         * @type number
+         */
         get height() { return this.yMax - this.yMin; }
 
+        /**
+         * Coordinates of the bbox in the form [xMin, yMin, xMax, yMax]
+         * @type number[]
+         */
         get coordinates() { return utils.copyArray(this._p); }
 
         /** @deprecated */
