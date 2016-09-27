e888969d9f206d2796dca1264c53e8fd5e190f0a
Maxim Gritsenko <maxim@gritsenko.biz>
2016-09-27 10:13:24 +0300

    sGis-107, sGis-108 sGis.controls.PolyDrag docs

--- source/controls/PolyDrag.js
+++ source/controls/PolyDrag.js
@@ -6,7 +6,20 @@ sGis.module('controls.PolyDrag', [
 
     'use strict';
 
+    /**
+     * Base class for controls that create polygon feature by dragging some area on the map. When the control is activated,
+     * a new temporary layer is created and added to the map. The feature is drawn on that temp layer. After drawing is
+     * finished, if the .activeLayer property is set, the feature is moved to the active layer.
+     * @alias sGis.controls.PolyDrag
+     * @extends sGis.Control
+     * @fires sGis.controls.PolyDrag#drawingBegin
+     * @fires sGis.controls.PolyDrag#drawingFinish
+     */
     class PolyDrag extends Control {
+        /**
+         * @param {sGis.Map} map - map the control will work with
+         * @param {Object} [properties] - key-value set of properties to be set to the instance
+         */
         constructor(map, properties) {
             super(map, properties);
 
@@ -70,6 +83,27 @@ sGis.module('controls.PolyDrag', [
         get activeFeature() { return this._activeFeature; }
     }
 
+    /**
+     * Symbol of the created features
+     * @member {sGis.Symbol} sGis.controls.PolyDrag#symbol
+     * @default new sGis.symbol.polygon.Simple()
+     */
+
     return PolyDrag;
+
+    /**
+     * Drawing of a new feature is started by starting a drag of the map.
+     * @event sGis.controls.PolyDrag#drawingBegin
+     * @type {Object}
+     * @mixes sGisEvent
+     */
+
+    /**
+     * Drawing is finished by finishing the drag and the feature is added to the active layer (if set).
+     * @event sGis.controls.PolyDrag#drawingFinish
+     * @type {Object}
+     * @mixes sGisEvent
+     * @ptop {sGis.feature.Polygon} feature - created feature
+     */
 
 });
