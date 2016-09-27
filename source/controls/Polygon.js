21c35acb756c909a7b18c712bd2b17df3af3b036
Maxim Gritsenko <maxim@gritsenko.biz>
2016-09-27 09:59:41 +0300

    sGis-105 sGis.controls.Polygon docs

--- source/controls/Polygon.js
+++ source/controls/Polygon.js
@@ -6,11 +6,26 @@ sGis.module('controls.Polygon', [
     
     'use strict';
 
+    /**
+     * Control for drawing polygon features.
+     * @alias sGis.control.Polyline
+     * @extends sGis.controls.Poly
+     */
     class PolygonControl extends Poly {
+        /**
+         * @param {sGis.Map} map - map the control will work with
+         * @param {Object} [properties] - key-value set of properties to be set to the instance
+         */
         constructor(map, properties) {
             super(Polygon, new PolygonSymbol(), map, properties);
         }
     }
+
+    /**
+     * Symbol of the created features
+     * @member {sGis.Symbol} sGis.controls.Polyline#symbol
+     * @default new sGis.symbol.polygon.Simple()
+     */
     
     return PolygonControl;
 
