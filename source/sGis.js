bede78489dda71f22ef049cd958797e9d0eaa432
Maxim Gritsenko <maxim@gritsenko.biz>
2016-11-15 15:56:03 +0300

    0.1.1

--- source/sGis.js
+++ source/sGis.js
@@ -6,8 +6,8 @@
      */
     var sGis = {};
 
-    sGis.version = "0.1.3";
-    sGis.releaseDate = "07.11.2016";
+    sGis.version = "0.1.1";
+    sGis.releaseDate = "15.11.2016";
 
     sGis.browser = (function() {
         var ua= navigator.userAgent,
