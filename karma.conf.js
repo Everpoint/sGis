f9b85a13c0280bb3406e1ecf9f4f2c02f876430b
TPoryadin <tporyadin@everpoint.ru>
2016-12-01 14:18:47 +0300

    Add jasmine-matchers plugin, add constructor tests for LayerGroup

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,15 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'jasmine-matchers'],
+
+    //Plugins
+    plugins: [
+      'karma-jasmine',
+      'karma-coverage',
+      'karma-chrome-launcher',
+      'karma-jasmine-matchers'
+    ],
 
 
     // list of files / patterns to load in the browser
