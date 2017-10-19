32dc0040ddba83fee1c3d3cb7d375cee879bb40f
Artem <attemka@gmail.com>
2017-10-19 11:22:14 +0300

    updated colors tests

--- test/utils/Color.js
+++ test/utils/Color.js
@@ -2,9 +2,9 @@ describe('sGis.utils.Color',() =>{
     let color;
  describe('setHsv', () =>{
      it('should correctly parse hsv color into rgb', () => {
-         expect(new sGis.utils.Color('#fdbcae').setHsv(120, 60, 30)).toEqual([31, 77, 31]);
-         expect(new sGis.utils.Color('#fdbcae').setHsv(360, 0, 100)).toEqual([255, 255, 255]);
-         expect(new sGis.utils.Color('#fdbcae').setHsv(15, 25, 45)).toEqual([115, 93, 86]);
+         expect(new sGis.utils.Color().setHsv(120, 60, 30)).toEqual([31, 77, 31]);
+         expect(new sGis.utils.Color().setHsv(360, 0, 100)).toEqual([255, 255, 255]);
+         expect(new sGis.utils.Color().setHsv(15, 25, 45)).toEqual([115, 93, 86]);
      });
      it('should correctly set hue, saturation and value', () =>{
          expect(new sGis.utils.Color('#fdbcae').h).toEqual(11);
