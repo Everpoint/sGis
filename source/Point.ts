0269649bc6aa0fba4b5ef7ff24251519cf1bb070
tanyaisinmybed <tporyadin@everpoint.ru>
2017-11-11 17:51:23 +0300

    Point's docs and types

--- source/Point.ts
+++ source/Point.ts
@@ -1,12 +1,27 @@
-import {Coordinates} from './baseTypes';
-import {Crs, geo} from './Crs';
-import {error} from './utils/utils';
-import {softEquals} from "./utils/math";
+import { Coordinates } from './baseTypes';
+import { Crs, geo } from './Crs';
+import { error } from './utils/utils';
+import { softEquals } from "./utils/math";
 
+/**
+ * IPoint interface
+ */
 export interface IPoint {
+    /**
+     * Position of this point
+     */
     position: Coordinates;
+    /**
+     * X coordinate of this point
+     */
     x: number;
+    /**
+     * Y coordinate of this point
+     */
     y: number;
+    /**
+     * Coordinate system of this point
+     */
     readonly crs: Crs;
 
     /**
@@ -20,22 +35,41 @@ export interface IPoint {
 /**
  * Simple geographical point
  * @alias sGis.Point
+ * @example const point = new Point([0, 0], CRS.geo)
  */
 export class Point implements IPoint {
-    private _crs: Crs;
+    /**
+     * Coordinate system of this point
+     */
+    readonly crs: Crs;
 
-    public position: Coordinates;
+    /**
+     * Position of this point
+     */
+    position: Coordinates;
 
+    /**
+     * @constructor
+     * @param position - point position
+     * @param crs - point coordinate system
+     */
     constructor(position: Coordinates, crs: Crs = geo) {
-        if (crs !== undefined) this._crs = crs;
+        this.crs = crs;
         this.position = position;
     }
 
+    /**
+     * Returns a new point with same position in new crs
+     * @param newCrs - target coordinate system
+     * @throws Cannot project to specified crs.
+     */
     projectTo(newCrs: Crs): Point {
-        let projection = this.crs.projectionTo(newCrs);
-        if (projection === null) error("Cannot project point to crs: " + newCrs.toString());
-
-        return new Point(projection(this.position), newCrs);
+        const projection = this.crs.projectionTo(newCrs);
+        if (projection) {
+            return new Point(projection(this.position), newCrs);
+        } else {
+            throw new Error("Cannot project point to crs: " + newCrs.toString());
+        }
     }
 
     /**
@@ -46,30 +80,36 @@ export class Point implements IPoint {
         return new Point(this.position, this.crs);
     }
 
+    /**
+     * Getter returns clone of this point. Setter set point position.
+     */
     get point() { return this.clone(); }
     set point(point) { this.position = point.projectTo(this.crs).position; }
 
+    /**
+     * X coordinate of this point
+     */
     get x() { return this.position[0]; }
     set x(x) { this.position[0] = x; }
 
+    /**
+     * Y coordinate of this point
+     */
     get y() { return this.position[1]; }
     set y(y) { this.position[1] = y; }
-
-    /**
-     * Coordinate system of the point
-     * @type sGis.Crs
-     */
-    get crs() { return this._crs; }
 
     /**
      * Returns true if the target point has the same position and crs as the current one
      * @param {sGis.Point} point - target point for comparison
      * @returns {boolean}
      */
-    equals(point) {
+    equals(point: Point) {
         return softEquals(point.x, this.x) && softEquals(point.y, this.y) && point.crs.equals(this.crs);
     }
 
+    /**
+     * Coordinates / position of this point
+     */
     get coordinates(): Coordinates { return [this.position[0], this.position[1]]; }
     set coordinates(position: Coordinates) { this.position = [position[0], position[1]]; }
 }
