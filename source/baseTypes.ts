127a48b7d0c914addfd0d2cd4f1218146e87c4c1
Maxim Gritsenko <maxim@gritsenko.biz>
2017-11-22 09:57:13 +0300

    Resolves #11

--- source/baseTypes.ts
+++ source/baseTypes.ts
@@ -1,5 +1,16 @@
+/**
+ * A pair of coordinates, usually [x, y] or [lon, lat].
+ */
 export type Coordinates = [number, number];
+
+/**
+ * A set of coordinate pairs that represent a set of points (form multipoint features), a polyline or a contour of a polygon.
+ */
 export type Contour = Coordinates[];
 
+/**
+ * Minimum and maximum resolutions respectively at which a layer (or a set of layers) should be displayed.
+ * If either value is negative (usually -1) that limit is not applied. Second value, if positive, must always be
+ * greater than the first one.
+ */
 export type ResolutionLimits = [number, number];
-
