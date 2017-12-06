207c545c7e361645b93062f8bb9f16d8be0c6662
Maxim Gritsenko <maxim@gritsenko.biz>
2017-12-06 11:28:30 +0300

    Typing of PolyTransform control. Resolves #22.

--- source/controls/PolyTransform.ts
+++ source/controls/PolyTransform.ts
@@ -1,50 +1,96 @@
 import {Control} from "./Control";
-import {FeatureLayer} from "../FeatureLayer";
 import {PointSymbol} from "../symbols/point/Point";
 import {SquareSymbol} from "../symbols/point/Square";
 import {Poly} from "../features/Poly";
 import {PointFeature} from "../features/Point";
 import {rotate, scale} from "../geotools";
 import {Coordinates} from "../baseTypes";
+import {Symbol} from "../symbols/Symbol";
+import {Map} from "../Map";
+import {DragEndEvent, DragEvent, DragStartEvent} from "../commonEvents";
+import {sGisEvent} from "../EventHandler";
+
+/**
+ * @event RotationStartEvent
+ */
+export class RotationStartEvent extends sGisEvent {
+    static type: string = 'rotationStart';
+
+    constructor() {
+        super(RotationStartEvent.type);
+    }
+}
+
+/**
+ * @event RotationEndEvent
+ */
+export class RotationEndEvent extends sGisEvent {
+    static type: string = 'rotationEnd';
+
+    constructor() {
+        super(RotationEndEvent.type);
+    }
+}
+
+/**
+ * @event ScalingStartEvent
+ */
+export class ScalingStartEvent extends sGisEvent {
+    static type: string = 'scalingStart';
+
+    constructor() {
+        super(ScalingStartEvent.type);
+    }
+}
+
+/**
+ * @event ScalingEndEvent
+ */
+export class ScalingEndEvent extends sGisEvent {
+    static type: string = 'scalingEnd';
+
+    constructor() {
+        super(ScalingEndEvent.type);
+    }
+}
 
 /**
  * Control for modifying polylines or polygons as whole. When activeFeature is set, it shows points around the feature
  * dragging which one can scale or rotate the feature.
  * @alias sGis.controls.PolyTransform
- * @extends sGis.Control
- * @fires sGis.controls.PolyTransform#rotationStart
- * @fires sGis.controls.PolyTransform#rotationEnd
- * @fires sGis.controls.PolyTransform#scalingStart
- * @fires sGis.controls.PolyTransform#scalingEnd
+ * @fires [[RotationStartEvent]]
+ * @fires [[RotationEndEvent]]
+ * @fires [[ScalingStartEvent]]
+ * @fires [[ScalingEndEvent]]
  */
 export class PolyTransform extends Control {
     /** Symbol of the rotation handle. */
-    rotationHandleSymbol = new PointSymbol({offset: [0, -30]});
+    rotationHandleSymbol: Symbol = new PointSymbol({offset: [0, -30]});
 
     /** Symbol of the scaling handles. */
-    scaleHandleSymbol = new SquareSymbol({ fillColor: 'transparent', strokeColor: 'black', strokeWidth: 2, size: 7 });
+    scaleHandleSymbol: Symbol = new SquareSymbol({ fillColor: 'transparent', strokeColor: 'black', strokeWidth: 2, size: 7 });
 
     /** Distance in pixels between scaling handles and feature bbox. */
-    scaleHandleOffset = 12;
+    scaleHandleOffset: number = 12;
 
     /** If set to false the rotation handle will not be displayed. */
-    enableRotation = true;
+    enableRotation: boolean = true;
 
     /** If set to false the scaling handle will not be displayed. */
-    enableScaling = true;
+    enableScaling: boolean = true;
 
-    ignoreEvents = false;
+    ignoreEvents: boolean = false;
 
-    private _activeFeature: Poly;
+    private _activeFeature: Poly | null;
     private _rotationHandle: PointFeature;
     private _scaleHandles: PointFeature[];
-    private _rotationBase: Coordinates;
+    private _rotationBase: Coordinates | null;
 
     /**
-     * @param {sGis.Map} map - map object the control will work with
-     * @param {Object} [options] - key-value set of properties to be set to the instance
+     * @param map - map object the control will work with
+     * @param __namedParameters - key-value set of properties to be set to the instance
      */
-    constructor(map, {activeLayer = null, isActive = false} = {}) {
+    constructor(map: Map, {activeLayer = null, isActive = false} = {}) {
         super(map, {activeLayer, useTempLayer: true});
 
         this._handleRotationStart = this._handleRotationStart.bind(this);
@@ -58,44 +104,45 @@ export class PolyTransform extends Control {
 
     /**
      * Feature to edit. If set to null, the control is deactivated.
-     * @type {sGis.feature.Poly}
      */
-    get activeFeature() { return this._activeFeature; }
-    set activeFeature(/** sGis.feature.Poly */ feature) {
+    get activeFeature(): Poly | null { return this._activeFeature; }
+    set activeFeature(feature: Poly | null) {
         this.deactivate();
         this._activeFeature = feature;
-        this.activate();
+        if (feature) this.activate();
     }
 
     /**
      * Updates position of the editor handles.
      */
-    update() { if (this._activeFeature) this._updateHandles(); }
+    update(): void { if (this._activeFeature) this._updateHandles(); }
 
-    _activate() {
+    protected _activate(): void {
         if (!this._activeFeature) return;
         this._setHandles();
     }
 
-    _deactivate() {
-        if (!this._activeFeature) return;
+    protected _deactivate(): void {
+        return;
     }
 
-    _setHandles() {
+    private _setHandles(): void {
         if (this.enableRotation) this._setRotationHandle();
         if (this.enableScaling) this._setScaleHandles();
     }
 
-    _setRotationHandle() {
+    private _setRotationHandle(): void {
         this._rotationHandle = new PointFeature([0, 0], {crs: this.map.crs, symbol: this.rotationHandleSymbol});
         this._updateRotationHandle();
-        this._rotationHandle.on('dragStart', this._handleRotationStart);
-        this._rotationHandle.on('drag', this._handleRotation);
-        this._rotationHandle.on('dragEnd', this._handleRotationEnd);
+
+        this._rotationHandle.on(DragStartEvent.type, this._handleRotationStart);
+        this._rotationHandle.on(DragEvent.type, this._handleRotation);
+        this._rotationHandle.on(DragEndEvent.type, this._handleRotationEnd);
+
         this._tempLayer.add(this._rotationHandle);
     }
 
-    _setScaleHandles() {
+    private _setScaleHandles(): void {
         this._scaleHandles = [];
         for (let i = 0; i < 9; i++) {
             if (i === 4) continue;
@@ -106,31 +153,32 @@ export class PolyTransform extends Control {
             symbol.offset = [this.scaleHandleOffset * xk, this.scaleHandleOffset * yk];
 
             this._scaleHandles[i] = new PointFeature([0, 0], {symbol: symbol, crs: this.map.crs});
-            this._scaleHandles[i].on('dragStart', this._handleScalingStart.bind(this, i));
-            this._scaleHandles[i].on('drag', this._handleScaling.bind(this, i));
-            this._scaleHandles[i].on('dragEnd', this._handleScalingEnd);
+
+            this._scaleHandles[i].on(DragStartEvent.type, this._handleScalingStart.bind(this, i));
+            this._scaleHandles[i].on(DragEvent.type, this._handleScaling.bind(this, i));
+            this._scaleHandles[i].on(DragEndEvent.type, this._handleScalingEnd);
         }
 
         this._tempLayer.add(this._scaleHandles);
         this._updateScaleHandles();
     }
 
-    _handleRotationStart(sGisEvent) {
+    private _handleRotationStart(event: DragStartEvent): void {
         if (this.ignoreEvents) return;
 
         this._rotationBase = this._activeFeature.bbox.center.projectTo(this.map.crs).position;
-        sGisEvent.draggingObject = this._rotationHandle;
-        sGisEvent.stopPropagation();
+        event.draggingObject = this._rotationHandle;
+        event.stopPropagation();
 
-        this.fire('rotationStart');
+        this.fire(new RotationStartEvent());
     }
 
-    _handleRotation(sGisEvent) {
-        let xPrev = sGisEvent.point.x + sGisEvent.offset.x;
-        let yPrev = sGisEvent.point.y + sGisEvent.offset.y;
+    private _handleRotation(event: DragEvent): void {
+        let xPrev = event.point.x + event.offset[0];
+        let yPrev = event.point.y + event.offset[1];
 
         let alpha1 = xPrev === this._rotationBase[0] ? Math.PI / 2 : Math.atan2(yPrev - this._rotationBase[1], xPrev - this._rotationBase[0]);
-        let alpha2 = sGisEvent.point.x === this._rotationBase[0] ? Math.PI / 2 : Math.atan2(sGisEvent.point.y - this._rotationBase[1], sGisEvent.point.x - this._rotationBase[0]);
+        let alpha2 = event.point.x === this._rotationBase[0] ? Math.PI / 2 : Math.atan2(event.point.y - this._rotationBase[1], event.point.x - this._rotationBase[0]);
         let angle = alpha2 - alpha1;
 
         rotate([this._activeFeature], angle, this._rotationBase, this.map.crs);
@@ -138,23 +186,23 @@ export class PolyTransform extends Control {
         this._updateHandles();
     }
 
-    _handleRotationEnd() {
-        this.fire('rotationEnd');
+    private _handleRotationEnd(): void {
+        this.fire(new RotationEndEvent());
     }
 
-    _updateHandles() {
+    private _updateHandles(): void {
         if (this.enableRotation) this._updateRotationHandle();
         if (this.enableScaling) this._updateScaleHandles();
 
         this._tempLayer.redraw();
     }
 
-    _updateRotationHandle() {
+    private _updateRotationHandle(): void {
         let bbox = this._activeFeature.bbox.projectTo(this.map.crs);
         this._rotationHandle.position = [(bbox.xMin + bbox.xMax)/2, bbox.yMax];
     }
 
-    _updateScaleHandles() {
+    private _updateScaleHandles(): void {
         let bbox = this._activeFeature.bbox.projectTo(this.map.crs);
         let xs = [bbox.xMin, (bbox.xMin + bbox.xMax)/2, bbox.xMax];
         let ys = [bbox.yMin, (bbox.yMin + bbox.yMax)/2, bbox.yMax];
@@ -165,16 +213,16 @@ export class PolyTransform extends Control {
         }
     }
 
-    _handleScalingStart(index, sGisEvent) {
+    private _handleScalingStart(index: number, event: DragStartEvent): void {
         if (this.ignoreEvents) return;
 
-        sGisEvent.draggingObject = this._scaleHandles[index];
-        sGisEvent.stopPropagation();
+        event.draggingObject = this._scaleHandles[index];
+        event.stopPropagation();
 
-        this.fire('scalingStart');
+        this.fire(new ScalingStartEvent());
     }
 
-    _handleScaling(index, sGisEvent) {
+    private _handleScaling(index: number, event: DragEvent): void {
         const MIN_SIZE = 10;
         let xIndex = index % 3;
         let yIndex = Math.floor(index / 3);
@@ -187,10 +235,10 @@ export class PolyTransform extends Control {
         let resolution = this.map.resolution;
         let tolerance = MIN_SIZE * resolution;
         let width = bbox.width;
-        let xScale = baseX === 1 ? 1 : (width + (baseX - 1) * sGisEvent.offset.x) / width;
+        let xScale = baseX === 1 ? 1 : (width + (baseX - 1) * event.offset[0]) / width;
         if (width < tolerance && xScale < 1) xScale = 1;
         let height = bbox.height;
-        let yScale = baseY === 1 ? 1 : (height + (baseY - 1) * sGisEvent.offset.y) / height;
+        let yScale = baseY === 1 ? 1 : (height + (baseY - 1) * event.offset[1]) / height;
         if (height < tolerance && yScale < 1) yScale = 1;
 
         scale([this._activeFeature], [xScale, yScale], basePoint, this.map.crs);
@@ -198,34 +246,7 @@ export class PolyTransform extends Control {
         this._updateHandles();
     }
 
-    _handleScalingEnd() {
-        this.fire('scalingEnd');
+    private _handleScalingEnd(): void {
+        this.fire(new ScalingEndEvent());
     }
 }
-/**
- * Rotation has started.
- * @event sGis.controls.PolyTransform#rotationStart
- * @type {Object}
- * @mixes sGisEvent
- */
-
-/**
- * Rotation has ended.
- * @event sGis.controls.PolyTransform#rotationEnd
- * @type {Object}
- * @mixes sGisEvent
- */
-
-/**
- * Scaling has started.
- * @event sGis.controls.PolyTransform#scalingStart
- * @type {Object}
- * @mixes sGisEvent
- */
-
-/**
- * Scaling has ended.
- * @event sGis.controls.PolyTransform#scalingEnd
- * @type {Object}
- * @mixes sGisEvent
- */
