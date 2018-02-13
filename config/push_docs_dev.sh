c4122442e1daba28b842e7a2157d69c47239c429
Maxim Gritsenko <maxim@gritsenko.biz>
2018-02-13 12:01:27 +0300

    Configuring dev docs deployment

---
+++ config/push_docs_dev.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ $TRAVIS_BRANCH == 'dev' ]]
+then
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+
+    git clone https://github.com/Everpoint/everpoint.github.io.git
+
+    cd everpoint.github.io
+    rm -vrf ./docs/dev/sgis
+    mkdir -vp ./docs/dev/sgis
+    cp -vr ../doc/* ./docs/dev/sgis
+
+    git add -A
+    git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+
+    git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+    git push --quiet
+
+    cd ../
+    rm -rf ./everpoint.github.io
+fi
