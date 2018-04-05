8e2c0b71fa50ff88582bd566e1b6a6b9fa656009
kreizo <thekreizo@gmail.com>
2018-04-05 10:27:43 +0300

    Feature Group

--- source/features/FeatureGroup.ts
+++ source/features/FeatureGroup.ts
@@ -3,22 +3,34 @@ import {IPoint, Point} from "../Point";
 import {Coordinates} from "../baseTypes";
 import {PointSymbol} from "../symbols/point/Point";
 import {Crs} from "../Crs";
-import {PointFeature} from "./PointFeature";
 import {Bbox} from "../Bbox";
+import {PointFeature} from "./PointFeature";
+import {LabelFeature} from "./Label";
 
 export class FeatureGroup extends Feature implements IPoint {
-    private _position: Coordinates;
     private _features: Feature[];
     private _bbox: Bbox;
 
     constructor(features, { symbol = new PointSymbol(), ...params }: FeatureParams = {}) {
         super({symbol, ...params});
-        this._features = features;
+        this._features = features.map(feature => {
+            if (this.crs.equals(feature.crs)) return feature;
+            else {
+                let projected = feature.projectTo(this.crs);
+                return new PointFeature(projected.position, { crs: this.crs, symbol: this.symbol });
+            }
+        });
     }
 
-    projectTo(crs: Crs): PointFeature {
-        let projected = Point.prototype.projectTo.call(this, crs);
-        return new PointFeature(projected.position, { crs: crs, symbol: this.symbol });
+    /**
+     * Returns a copy of the feature. Only generic properties are copied.
+     */
+    clone() {
+        return new FeatureGroup(this._features, {crs: this.crs, symbol: this.symbol});
+    }
+
+    projectTo(crs: Crs): FeatureGroup {
+        return new FeatureGroup(this._features, { crs: crs, symbol: this.symbol });
     }
 
     features(): Feature[] {
@@ -26,17 +38,15 @@ export class FeatureGroup extends Feature implements IPoint {
     }
 
     get position(): Coordinates {
-        const coordinates = [0, 0];
+        let x:number = 0;
+        let y:number = 0 ;
 
         for (let i = 0; i < this._features.length; i++) {
-            coordinates[0] += this._features[i].centroid[0];
-            coordinates[1] += this._features[i].centroid[1];
+            x += this._features[i].centroid[0];
+            y += this._features[i].centroid[1];
         }
 
-        return [coordinates[0] / this._features.length, coordinates[1] / this._features.length];
-    }
-    set position(position: Coordinates) {
-        this._position = [position[0], position[1]];
+        return [x / this._features.length, y / this._features.length];
     }
 
     get bbox(): Bbox {
@@ -60,22 +70,13 @@ export class FeatureGroup extends Feature implements IPoint {
     get point(): Point { return new Point(this.position, this.crs); }
 
     get x(): number { return this.position[0]; }
-    set x(x: number) {
-        this.position[0] = x;
-        this.redraw();
-    }
 
     get y(): number { return this.position[1]; }
-    set y(y: number) {
-        this.position[1] = y;
-        this.redraw();
-    }
 
     /**
      * @deprecated
      */
     get coordinates(): Coordinates { return [this.position[0], this.position[1]]; }
-    set coordinates(position: Coordinates) { this.position = [position[0], position[1]]; }
 
     get centroid(): Coordinates { return this.position; }
 }
