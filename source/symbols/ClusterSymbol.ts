238964e08633d4ece720fb1a09b88256d97ded02
kreizo <thekreizo@gmail.com>
2018-04-10 07:30:43 +0300

    ED-2635
    clone node

--- source/symbols/ClusterSymbol.ts
+++ source/symbols/ClusterSymbol.ts
@@ -158,7 +158,8 @@ export class ClusterSymbol extends DynamicPointSymbol {
         }
 
         if (this.node) {
-            wrapper.appendChild(this.node);
+            const cloneNode = this.node.cloneNode();
+            wrapper.appendChild(cloneNode);
             wrapper.style.borderWidth = '0';
         } else {
             const radius = this.size / 2 - this.borderWidth;
