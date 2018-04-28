c9e0a518f8b809ff9bd5d60e324b31d24270f871
kreizo <thekreizo@gmail.com>
2018-04-28 11:06:10 +0300

    ED-2634
    refactoring

--- source/layers/clusterProviders/GridClusterProvider.ts
+++ source/layers/clusterProviders/GridClusterProvider.ts
@@ -9,107 +9,53 @@ export interface IClusterProvider {
     resolution?: number;
 }
 
-export interface FeatureCluster extends Feature {
-    indexX?: number;
-    indexY?: number;
-}
-
 export class GridClusterProvider {
     private _features: Feature[];
     private _size: number;
     private _resolution: number;
-    private _clusters: FeatureGroup[];
-    private _cache:  { [key: string]: FeatureGroup[]; };
+    private _cache: FeatureGroup[];
 
     constructor({ size = 44 }: IClusterProvider = {}) {
         this._features = [];
         this._size = size;
         this._resolution = 0;
-        this._clusters = [];
-        this._cache = {};
-    }
-
-    private _groupByIndex(features: Feature[], bbox: Bbox): FeatureGroup[] {
-        const groups: { [key: string]: FeatureCluster[] } = {};
-        const f = (feature: FeatureCluster) => [feature.indexX, feature.indexY];
-
-        features.forEach((feature: FeatureCluster) => {
-            const group = JSON.stringify(f(feature));
-            groups[group] = groups[group] || [];
-            groups[group].push(feature);
-        });
-
-        return Object.keys(groups).map( (group) => {
-            return new FeatureGroup(groups[group], { crs: bbox.crs });
-        })
-    }
-
-    private _pythagoras(p1: FeatureGroup, p2: FeatureGroup): number {
-        return Math.hypot(p2.centroid[0] - p1.centroid[0], p2.centroid[1] - p1.centroid[1]);
-    }
-
-    private _compareGroupsByDistance(group: FeatureGroup[], bbox: Bbox): any {
-        const size = this._size * this._resolution;
-        const clustered: FeatureGroup[] = [];
-        let checkDistance = false;
-
-        for (let i = 0; i < group.length; i++) {
-            const cluster: Feature[] = group[i].features;
-            let newGroup = false;
-
-            for (let j = i + 1; j < group.length; j++) {
-                if (this._pythagoras(group[i], group[j]) < size) {
-                    checkDistance = true;
-                    newGroup = true;
-                    cluster.push(...group[j].features);
-                    group.splice(j, 1);
-                }
-            }
-
-            if (newGroup) clustered.push(new FeatureGroup(cluster, { crs: bbox.crs }));
-            else clustered.push(group[i]);
-        }
-        return {
-            checkDistance,
-            clustered,
-        };
+        this._cache = [];
     }
 
     getClusters(bbox: Bbox, resolution: number): FeatureGroup[] {
-        if (this._cache[resolution]) return this._cache[resolution];
-
-        if (this._resolution !== resolution && !this._cache[resolution]) {
+        if (this._resolution !== resolution) {
+            this._cache = [];
             this._resolution = resolution;
             const size = this._size * resolution;
-            const indexedFeatures = this._features.map(feature => {
-                const point = feature.projectTo(bbox.crs);
+
+            const groups: { [key: string]: Feature[] } = {};
+
+            for (let i = 0; i < this._features.length; i++) {
+                const point = this._features[i].projectTo(bbox.crs);
                 const indexX = Math.round(point.centroid[0] / size);
                 const indexY = Math.round(point.centroid[1] / size);
-                return Object.assign(feature, { indexX, indexY });
-            });
-
-            let flag: boolean = true;
-            let clusters: FeatureGroup[] = this._groupByIndex(indexedFeatures, bbox);
-
-            while (flag) {
-                const { clustered, checkDistance } = this._compareGroupsByDistance(clusters, bbox);
-                clusters = clustered;
-                flag = checkDistance;
+                if (groups[`${indexX}${indexY}`]) {
+                    groups[`${indexX}${indexY}`] = groups[`${indexX}${indexY}`].concat(
+                        this._features[i],
+                    );
+                } else groups[`${indexX}${indexY}`] = [this._features[i]];
             }
 
-            this._cache = {
-                ...this._cache,
-                [resolution]: clusters,
-            }
-            this._clusters = clusters;
+            this._cache = Object.keys(groups).map(
+                group => new FeatureGroup(groups[group], { crs: bbox.crs }),
+            );
         }
 
-        return this._clusters;
+        return this._cache.filter(
+            feature =>
+                feature.crs.canProjectTo(bbox.crs) &&
+                (feature.persistOnMap || feature.bbox.intersects(bbox)),
+        );
     }
 
     add(features: Feature | Feature[]): void {
-        this._cache = {}
-        this._clusters = [];
+        this._cache = [];
+
         const toAdd = Array.isArray(features) ? features : [features];
         if (toAdd.length === 0) return;
         toAdd.forEach(f => {
@@ -119,8 +65,8 @@ export class GridClusterProvider {
     }
 
     remove(features: Feature | Feature[]): void {
-        this._cache = {}
-        this._clusters = [];
+        this._cache = [];
+
         const toRemove = Array.isArray(features) ? features : [features];
         if (toRemove.length === 0) return;
         toRemove.forEach(f => {
--- test/layers/clusterProviders/GridClusterProvider.ts
+++ test/layers/clusterProviders/GridClusterProvider.ts
@@ -1,6 +1,4 @@
 import {GridClusterProvider} from "../../../source/layers/clusterProviders/GridClusterProvider";
-import {FeatureGroup} from "../../../source/features/FeatureGroup";
-import {Feature} from "../../../source/features/Feature";
 import {PointFeature} from "../../../source/features/PointFeature";
 import {wgs84, webMercator} from "../../../source/Crs";
 import {Bbox} from "../../../source/Bbox";
@@ -18,10 +16,6 @@ describe('GridClusterProvider', () => {
     const clusterProvider = new GridClusterProvider({
         size: clusterSize,
     });
-
-    const pythagoras = (p1: Feature, p2: Feature): number => {
-        return Math.hypot(p2.centroid[0] - p1.centroid[0], p2.centroid[1] - p1.centroid[1]);
-    }
 
     const objects = [
         [73.355357, 54.878128],
@@ -43,55 +37,16 @@ describe('GridClusterProvider', () => {
 
     const clusters = clusterProvider.getClusters(bbox, resolution);
 
-    it('.getClusters() should return 4 featureGroup', () => {
-        expect(clusters.length).toBe(4);
+    it('.getClusters() should return 3 featureGroup', () => {
+        expect(clusters.length).toBe(3);
     });
 
-    it('.getClusters() should return contains the same length', () => {
-        const sumFeaturesLength = clusters.reduce((prev: number, curr: FeatureGroup) => {
-            return prev + curr.features.length;
-        }, 0);
-
-        expect(points.length).toBe(sumFeaturesLength);
-    });
-
-    it('The distance between clusters does not exceed the specified size', () => {
-        let distanceIsExceeded = false;
-
-        for (let i = 0; i < clusters.length; i++) {
-            for (let j = i + 1; j < clusters.length; j++) {
-                if (pythagoras(clusters[i], clusters[j]) < clusterSize * resolution)
-                    distanceIsExceeded = true;
-            }
-        }
-
-        expect(distanceIsExceeded).toBe(false);
-    });
-
-    it('property check', () => {
-        interface SecretPoint extends PointFeature {
-            name?: string;
-        }
-        const secretPoint: SecretPoint = new PointFeature([150.834439, 59.544667], {crs: wgs84});
-        const pointName = 'Секретная точка';
-        secretPoint.name = pointName;
-        clusterProvider.add(secretPoint);
-
-        const clusters = clusterProvider.getClusters(bbox, 9594);
-
-        interface MbSecretPoint extends Feature {
-            name?: string;
-        }
-
-        const mbSecretPoint: MbSecretPoint = clusters[2].features[3];
-
-        expect(clusters.length).toBe(4);
-        expect(clusters[2].features.length).toBe(4);
-        expect(mbSecretPoint.name).toBe(pointName);
+    it('.getClusters() should return contains 9 object', () => {
+        expect(points.length).toBe(9);
     });
 
     it('getClusters with changing resolution', () => {
-        expect(clusterProvider.getClusters(bbox, resolution).length).toBe(4);
-        expect(clusterProvider.getClusters(bbox, 4444).length).toBe(6);
+        expect(clusterProvider.getClusters(bbox, resolution).length).toBe(3);
+        expect(clusterProvider.getClusters(bbox, 4444).length).toBe(5);
     });
 });
