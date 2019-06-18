5df9ddfdc652adda2bf28b3d9c86be0c4b9c8cc9
tporyadin <tporyadin@everpoint.ru>
2019-06-18 17:27:42 +0300

    0.4.12

--- source/sGis.ts
+++ source/sGis.ts
@@ -66,8 +66,8 @@ export const event = eventModule;
 export {Bbox} from "./Bbox";
 export {Control} from "./controls/Control";
 
-export const version = "0.4.11";
-export const releaseDate = "21.05.2019";
+export const version = "0.4.12";
+export const releaseDate = "18.06.2019";
 
 let utilsModulesExt = <any>{};
 Object.assign(utilsModulesExt, utilsModule, { Color: Color });
