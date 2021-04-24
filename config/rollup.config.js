7418bc81afeb24f0aff1c1caa172f9a4b937fd57
svolkov <svolkov@everpoint.ru>
2021-04-24 10:50:22 +0300

    chore: remove commented rollup config

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -5,24 +5,5 @@ export default {
         format: 'umd',
         name: 'sGis',
         sourcemap: true
-    },
-    // plugins: [
-    //     typescript({
-    //         typescript: require('typescript'),
-    //         cacheRoot: `./.rts2_cache_esm`,
-    //         useTsconfigDeclarationDir: true,
-    //         tsconfigDefaults: {
-    //             compilerOptions: {
-    //                 sourceMap: true,
-    //                 declaration: true,
-    //                 declarationDir: './dist',
-    //             },
-    //         },
-    //         tsconfigOverride: {
-    //             compilerOptions: {
-    //                 target: 'esnext',
-    //             },
-    //         },
-    //     }),
-    // ]
+    }
 }
